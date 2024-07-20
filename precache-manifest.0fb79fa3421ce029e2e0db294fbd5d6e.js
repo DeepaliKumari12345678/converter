ff54cebd88b49bb88d79ab774dcfd14913a8f7b9
Deepali Kumari <129479284+DeepaliKumari12345678@users.noreply.github.com>
2024-07-20 16:43:58 +0530

    Add files via upload

---
+++ precache-manifest.0fb79fa3421ce029e2e0db294fbd5d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2519a39edf799298ac0ee09b2ba4d8fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9ac59bb9409838d82f4",
+    "url": "/static/css/main.9e385f43.chunk.css"
+  },
+  {
+    "revision": "2c97e1e1f10898f872cb",
+    "url": "/static/js/2.5a17e8bd.chunk.js"
+  },
+  {
+    "revision": "de6bb9b182cb4883b744809bb1ef7215",
+    "url": "/static/js/2.5a17e8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "e9ac59bb9409838d82f4",
+    "url": "/static/js/main.01020270.chunk.js"
+  },
+  {
+    "revision": "9ce659da1e767b4050ed",
+    "url": "/static/js/runtime-main.d30e4eab.js"
+  }
+]);
